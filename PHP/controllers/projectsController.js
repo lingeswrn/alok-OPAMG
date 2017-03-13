c02c82166935d1f48329ca9018114e4178f4f146
lingeswrn <lingeswrn@gmail.com>
2017-03-13 19:28:11 +0530

    popup widow

--- PHP/controllers/projectsController.js
+++ PHP/controllers/projectsController.js
@@ -1,10 +1,11 @@
-app.controller("projectsController", function( $scope, project , $location, measurements, $compile, layers){
+app.controller("projectsController", function( $scope, project , $location, measurements, $compile, layers, equ){
     $scope.projectData = {};
 	$scope.projectId = '';
 	$scope.measure = {};
     $scope.previousData = {};
     $scope.mappingLayer = {};
-    
+    $scope.equipent = {};
+	
 	$scope.switchForm1 = true;
 	$scope.switchForm2 = false;
 		
@@ -132,25 +133,39 @@ app.controller("projectsController", function( $scope, project , $location, meas
 			//marker.setPosition(map.getCenter());
 			var latitude = map.getCenter().lat().toFixed(6);
 			var longitude = map.getCenter().lng().toFixed(6);
-			$scope.value = $scope.project(map.getCenter());
-			$scope.measure.latitude = latitude;
-			$scope.measure.longitude = longitude;
-			$scope.measure.easting = $scope.value.x;
-			$scope.measure.northing = $scope.value.y;
+			$scope.value = GeogToUTM(map.getCenter());
+			console.log($scope.value)
+			$scope.measure.latitude = $scope.value.latitude.D +", "+ $scope.value.latitude.M +", "+$scope.value.latitude.S;
+			$scope.measure.longitude = $scope.value.longitude.D +", "+ $scope.value.longitude.M +", "+$scope.value.longitude.S;
+			$scope.measure.easting = $scope.value.easting;
+			$scope.measure.northing = $scope.value.northing;
 			$scope.$apply();
 			google.maps.event.trigger(map, "resize");
 		});
 		google.maps.event.addListener(marker, "dragend", function(mapEvent) {
-			$scope.value = $scope.project(mapEvent.latLng);
-			$scope.measure.easting = $scope.value.x;
-			$scope.measure.northing = $scope.value.y;
+			$scope.value = GeogToUTM(mapEvent.latLng);
+			console.log($scope.value);
+			$scope.measure.easting = $scope.value.easting;
+			$scope.measure.northing = $scope.value.northing;
 			$scope.$apply();
 			map.panTo(mapEvent.latLng);
 		});
                 
-				$scope.names = ["john", "bill", "charlie", "robert", "alban", "oscar", "marie", "celine", "brad", "drew", "rebecca", "michel", "francis", "jean", "paul", "pierre", "nicolas", "alfred", "gerard", "louis", "albert", "edouard", "benoit", "guillaume", "nicolas", "joseph"];
-                //var contentString = "<div><div class='form-group'> <label>Layer Library</label> <select class='form-control' id='layer'><option value='ELECTRICAL POLE 220 V' >ELECTRICAL POLE 220 V</option><option  value='ELECTRICAL POLE 440 V' >ELECTRICAL POLE 440 V</option><option  value='ELECTRICAL POLE 11 KV' >ELECTRICAL POLE 11 KV</option><option  value='ELECTRICAL POLE 33 KV' >ELECTRICAL POLE 33 KV</option><option  value='ELECTRICAL POLE 110 KV' >ELECTRICAL POLE 110 KV</option><option  value='ELECTRICAL POLE 220 KV' >ELECTRICAL POLE 220 KV</option><option  value='TRANSFERMER' >TRANSFERMER</option><option  value='N…..NO OF SAVE DATA IN LIST' >N…..NO OF SAVE DATA IN LIST</option></select> </div><div class='form-group col-sm-4'> <label>Back Site</label> <input class='form-control' type='text' placeholder='Back Site' id='back_site' value='0' ></div><div class='form-group col-sm-4'> <label>Intermediate Site</label> <input class='form-control' type='text' placeholder='Intermediate Site' id='intermediate_site' value='0'> </div><div class='form-group col-sm-4'> <label>Fore Site</label> <input class='form-control' type='text' placeholder='Fore Site' id='fore_site' value='0' ></div><button class='btn btn-primary waves-effect waves-light' ng-click='switchProject()'>Next</button></div>";
-                var contentString = "<form name='opamgForm'><div class='form-group'><angucomplete-alt id='members'placeholder='Search Layers'pause='400'selected-object='testObj'remote-url='http://localhost/alok/PHP/server/getLayers.php?s='remote-url-data-field='data'title-field='code'description-field='description'minlength='1'input-class='form-control form-control-small' ng-model='measure.layer_name'/></div><div class='form-group col-sm-4'><label>Back Site</label><input autocomplete=off class=form-control name=back_site ng-blur=calHight(); ng-model='measure.back_site' placeholder='Back Site' required></div><div class='form-group col-sm-4'><label>Intermediate Site</label><input autocomplete='off' class='form-control' name='intermediate_site' ng-blur='calReduceLevel();calHight();' ng-model='measure.intermediate_site' placeholder='Intermediate Site' required></div><div class='form-group col-sm-4'><label>Fore Site</label><input autocomplete='off' class='form-control' name='fore_site' ng-blur='calReduceLevel()' ng-model='measure.fore_site' placeholder='Fore Site' required></div><button class='btn btn-primary waves-effect waves-light' ng-click='switchProject()' ng-disabled='opamgForm.$invalid'>Next</button></form>";
+                var contentString = "<form name='opamgForm'>"				
+				contentString += "<div class='form-group col-sm-3'><label>Mapping CH</label><input autocomplete='off' class='form-control' name='intermediate_site' ng-model='measure.ch' placeholder='Mapping CH' required></div>"
+				contentString += "<div class='form-group col-sm-3'><label>Measurment CH</label><input autocomplete='off' class='form-control' name='intermediate_site' ng-model='measure.measurement_ch' placeholder='Intermediate Site' required></div>"
+				contentString += "<div class='form-group col-sm-3'><label>GPS Offset Length</label><input autocomplete='off' class='form-control' name='intermediate_site' ng-model='measure.offset_length' placeholder='Intermediate Site' required></div>"
+				contentString += "<div class='form-group col-sm-3'><label>N offset, E offset</label><input autocomplete='off' class='form-control' name='intermediate_site' ng-model='measure.intermediate_site' placeholder='' required style='height: 20px;'><input autocomplete='off' class='form-control' name='intermediate_site' ng-model='measure.intermediate_site' placeholder='' required style='height: 19px;'></div>"
+				contentString += "<div class='form-group col-sm-6'><input autocomplete='off' class='form-control' name='intermediate_site' ng-model='previousData.lSection' placeholder='L-Section Offset' required></div>"
+				contentString += "<div class='form-group col-sm-6'><input autocomplete='off' class='form-control' name='intermediate_site' ng-model='previousData.xSection' placeholder='X-Section Offset' required></div>"
+				contentString += "<div class='form-group'><angucomplete-alt id='members'placeholder='Search Layers'pause='400'selected-object='testObj'remote-url='server/getLayers.php?s='remote-url-data-field='data'title-field='code'description-field='description'minlength='1'input-class='form-control form-control-small' ng-model='measure.layer_name'/></div>"
+				contentString += "<div class='form-group col-sm-4'><input autocomplete=off class=form-control name=back_site ng-blur=calHight(); ng-model='measure.back_site' placeholder='Back Site' required></div>"
+				contentString += "<div class='form-group col-sm-4'><input autocomplete='off' class='form-control' name='intermediate_site' ng-blur='calReduceLevel();calHight();' ng-model='measure.intermediate_site' placeholder='Intermediate Site' required></div>"
+				contentString += "<div class='form-group col-sm-4'><input autocomplete='off' class='form-control' name='fore_site' ng-blur='calReduceLevel()' ng-model='measure.fore_site' placeholder='Fore Site' required></div>"
+				contentString += "<div class='form-group col-sm-6'><input autocomplete='off' class='form-control' name='intermediate_site' ng-model='previousData.lSection' placeholder='DGPS Filename'></div>"
+				contentString += "<div class='form-group col-sm-6'><input autocomplete='off' class='form-control' name='intermediate_site' ng-model='previousData.xSection' placeholder='EL'></div>"
+				contentString += "<button class='btn btn-primary waves-effect waves-light' ng-click='switchProject()' ng-disabled='opamgForm.$invalid'>Next</button>"
+				contentString += "</form>";
 
                     var infowindow = new google.maps.InfoWindow();
                      /*marker.addListener('click', function() {
@@ -298,8 +313,193 @@ app.controller("projectsController", function( $scope, project , $location, meas
 	$scope.exportToCSV = function(){
 		$("#projects").tableToCSV();
 	}
+	
+	$scope.listEquipment = function(){ 
+        $scope.equipent.action = "list";
+        equ.commonFun( $scope.equipent ).then( function(response){
+            if( response.data.code == '200'){
+               $scope.equipmentList = response.data.data;
+            }else{
+                
+            }
+        });
+    } 
+	
+	$scope.readEquipment = function( val ){
+		$scope.equipment = {};
+		var data = angular.fromJson(val);
+		$scope.equipment.model_number = data.model_number;
+		$scope.equipment.last_calibration_service_center = data.last_calibration_service_center;
+		$scope.equipment.least_count = data.least_count;
+		$scope.equipment.expiry_date = data.expiry_date;
+		$scope.equipment.owner = data.owner;
+		console.log($scope.equipment)
+	}
+	
+	function DDtoDMS(){
+	//Input= xd(long) and yd(lat)
+	//Output = xdd xm xs (long) and ydd ym ys (lat)
+	ydd = Math.floor(Math.abs(yd));
+	ym = Math.floor(60*(Math.abs(yd) - ydd));
+	ys = 3600*(Math.abs(yd)-ydd - ym/60);
+	if (yd<0){ydd=-ydd;}
+	xdd = Math.floor(Math.abs(xd));
+	xm = Math.floor(60*(Math.abs(xd) - xdd));
+	xs = 3600*(Math.abs(xd)-xdd - xm/60);
+	if (xd<0){xdd=-xdd;}
+	}//End DDtoDMS
+
+	
+	function Declarations(){
+//Symbols as used in USGS PP 1395: Map Projections - A Working Manual
+	DatumEqRad = [6378137.0,6378137.0,6378137.0,6378135.0,6378160.0,6378245.0,6378206.4,
+	6378388.0,6378388.0,6378249.1,6378206.4,6377563.4,6377397.2,6377276.3];	
+	DatumFlat = [298.2572236, 298.2572236, 298.2572215,	298.2597208, 298.2497323, 298.2997381, 294.9786982,
+	296.9993621, 296.9993621, 293.4660167, 294.9786982, 299.3247788, 299.1527052, 300.8021499]; 
+	Item = 0;//Default
+	k0 = 0.9996;//scale on central meridian
+	a = DatumEqRad[Item];//equatorial radius, meters. 
+	f = 1/DatumFlat[Item];//polar flattening.
+	b = a*(1-f);//polar axis.
+	e = Math.sqrt(1 - b*b/a*a);//eccentricity
+	drad = Math.PI/180;//Convert degrees to radians)
+	latd = 0;//latitude in degrees
+	phi = 0;//latitude (north +, south -), but uses phi in reference
+	e0 = e/Math.sqrt(1 - e*e);//e prime in reference
+	N = a/Math.sqrt(1-Math.pow(e*Math.sin(phi)),2);
+	T = Math.pow(Math.tan(phi),2);
+	C = Math.pow(e*Math.cos(phi),2);
+	lng = 0;//Longitude (e = +, w = -) - can't use long - reserved word
+	lng0 = 0;//longitude of central meridian
+	lngd = 0;//longitude in degrees
+	M = 0;//M requires calculation
+	x = 0;//x coordinate
+	y = 0;//y coordinate
+	k = 1;//local scale
+	utmz = 30;//utm zone
+	zcm = 0;//zone central meridian
+	DigraphLetrsE = "ABCDEFGHJKLMNPQRSTUVWXYZ";
+	DigraphLetrsN = "ABCDEFGHJKLMNPQRSTUV";
+	/* document.getElementById("EqRadBox").value = a;
+	document.getElementById("PolRadBox").value = b;
+	document.getElementById("FlatBox").value = f;
+	document.getElementById("RecipBox").value = 1/f; */
+	OOZok = false;
+	}//Close declarations
+
+	
+	function GeogToUTM(latlng){
+	//Convert Latitude and Longitude to UTM
+	Declarations();
+	k0 = 0.9996;//scale on central meridian
+	b = a*(1-f);//polar axis.
+	//alert(a+"   "+b);
+	//alert(1-(b/a)*(b/a));
+	e = Math.sqrt(1 - (b/a)*(b/a));//eccentricity
+	//alert(e);
+	//Input Geographic Coordinates
+	//Decimal Degree Option
+    latd0 = parseFloat(latlng.lat());
+	lngd0 = parseFloat(latlng.lng());
+	
+	/* latd1 = Math.abs(parseFloat(document.getElementById("DLatBox0").value));
+	latd1 = latd1 + parseFloat(document.getElementById("MLatBox0").value)/60;
+	latd1 = latd1 + parseFloat(document.getElementById("SLatBox0").value)/3600;
+	if (parseFloat(document.getElementById("DLatBox0").value)<0){latd1=-latd1;}
+	lngd1 = Math.abs(parseFloat(document.getElementById("DLonBox0").value));
+	lngd1 = lngd1 + parseFloat(document.getElementById("MLonBox0").value)/60;
+	lngd1 = lngd1 + parseFloat(document.getElementById("SLonBox0").value)/3600;
+	if (parseFloat(document.getElementById("DLonBox0").value)<0){lngd1=-lngd1;} */
+
+	lngd=lngd0;
+	latd=latd0;
+	/* if(isNaN(latd)){
+	latd = latd1;
+	document.getElementById("DDLatBox0").value = Math.floor(1000000*latd)/1000000;
+	lngd=lngd1;
+	document.getElementById("DDLonBox0").value = Math.floor(1000000*lngd)/1000000;
+	} */
+	
+	if(isNaN(lngd)){lngd = latd1;}
+		if(isNaN(latd)|| isNaN(lngd)){
+		alert("Non-Numeric Input Value");
+		}
+	if(latd <-90 || latd> 90){
+		alert("Latitude must be between -90 and 90");
+		}
+	if(lngd <-180 || lngd > 180){
+		alert("Latitude must be between -180 and 180");
+		}
+
+	xd = lngd;
+	yd = latd;
+	DDtoDMS();
+	//Read Input from DMS Boxes
+	/* document.getElementById("DLatBox0").value = Math.floor(ydd);
+	document.getElementById("MLatBox0").value = ym;
+	document.getElementById("SLatBox0").value = Math.floor(1000*ys)/1000;
+	
+	document.getElementById("DLonBox0").value = Math.floor(xdd);
+	document.getElementById("MLonBox0").value = xm;
+	document.getElementById("SLonBox0").value = Math.floor(1000*xs)/1000; */
+
+	
+	phi = latd*drad;//Convert latitude to radians
+	lng = lngd*drad;//Convert longitude to radians
+	utmz = 1 + Math.floor((lngd+180)/6);//calculate utm zone
+	latz = 0;//Latitude zone: A-B S of -80, C-W -80 to +72, X 72-84, Y,Z N of 84
+	if (latd > -80 && latd < 72){latz = Math.floor((latd + 80)/8)+2;}
+	if (latd > 72 && latd < 84){latz = 21;}
+	if (latd > 84){latz = 23;}
+		
+	zcm = 3 + 6*(utmz-1) - 180;//Central meridian of zone
+	//alert(utmz + "   " + zcm);
+	//Calculate Intermediate Terms
+	e0 = e/Math.sqrt(1 - e*e);//Called e prime in reference
+	esq = (1 - (b/a)*(b/a));//e squared for use in expansions
+	e0sq = e*e/(1-e*e);// e0 squared - always even powers
+	//alert(esq+"   "+e0sq)
+	N = a/Math.sqrt(1-Math.pow(e*Math.sin(phi),2));
+	//alert(1-Math.pow(e*Math.sin(phi),2));
+	//alert("N=  "+N);
+	T = Math.pow(Math.tan(phi),2);
+	//alert("T=  "+T);
+	C = e0sq*Math.pow(Math.cos(phi),2);
+	//alert("C=  "+C);
+	A = (lngd-zcm)*drad*Math.cos(phi);
+	//alert("A=  "+A);
+	//Calculate M
+	M = phi*(1 - esq*(1/4 + esq*(3/64 + 5*esq/256)));
+	M = M - Math.sin(2*phi)*(esq*(3/8 + esq*(3/32 + 45*esq/1024)));
+	M = M + Math.sin(4*phi)*(esq*esq*(15/256 + esq*45/1024));
+	M = M - Math.sin(6*phi)*(esq*esq*esq*(35/3072));
+	M = M*a;//Arc length along standard meridian
+	
+	M0 = 0;//M0 is M for some origin latitude other than zero. Not needed for standard UTM
+	//alert("M    ="+M);
+	//Calculate UTM Values
+	x = k0*N*A*(1 + A*A*((1-T+C)/6 + A*A*(5 - 18*T + T*T + 72*C -58*e0sq)/120));//Easting relative to CM
+	x=x+500000;//Easting standard 
+	y = k0*(M - M0 + N*Math.tan(phi)*(A*A*(1/2 + A*A*((5 - T + 9*C + 4*C*C)/24 + A*A*(61 - 58*T + T*T + 600*C - 330*e0sq)/720))));//Northing from equator
+	yg = y + 10000000;//yg = y global, from S. Pole
+	if (y < 0){y = 10000000+y;}
+	
+	//Output into UTM Boxes
+	//document.getElementById("UTMzBox1").value = utmz;
+	//document.getElementById("UTMeBox1").value = Math.round(10*(x))/10;
+	//document.getElementById("UTMnBox1").value = Math.round(10*y)/10;
+	
+	return {'northing': (10*(x)/10).toFixed(3), 'easting': (10*(y)/10).toFixed(3), 'latitude':{'D': Math.floor(ydd), 'M': ym, 'S': Math.floor(1000*ys)/1000} , 'longitude':{'D': Math.floor(xdd), 'M': xm, 'S': Math.floor(1000*xs)/1000} };
+	
+				
+}//close Geog to UTM
+
+
+	
+	
     $scope.project_list();
 	$scope.listLayers();
+	$scope.listEquipment();
     getGeolocation();
 });
 
